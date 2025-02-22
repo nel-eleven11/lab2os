9cbc8369474ff6540db5ea7d14ddfac7fc801c3c
Nelson Eduardo García Bravatti <104164145+nel-eleven11@users.noreply.github.com>
2025-02-21 23:06:24 -0600

    feat: code for ipcRunner.c

--- ipcRunner.c
+++ ipcRunner.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
+int main() {
+    pid_t pid = fork();
+
+    if (pid < 0) {
+        perror("Error al hacer fork()");
+        return 1;
+    }
+
+    if (pid == 0) {
+        // Proceso hijo: Ejecuta ipc con x = '#'
+        execl("./ipc", "./ipc", "5", "#", NULL);
+        perror("Error en execl (hijo)");
+        return 1;
+    } else {
+        // Proceso padre: Ejecuta ipc con x = '@'
+        execl("./ipc", "./ipc", "3", "@", NULL);
+        perror("Error en execl (padre)");
+        return 1;
+    }
+
+    return 0;
+}

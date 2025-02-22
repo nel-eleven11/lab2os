51649836949afb680bc4d24964e52210ab993a61
Nelson Eduardo García Bravatti <104164145+nel-eleven11@users.noreply.github.com>
2025-02-21 20:36:20 -0600

    feat: interation index added

--- ej21.c
+++ ej21.c
@@ -4,26 +4,29 @@
 int main() {
     clock_t start, end;
     double cpu_time_used;
-    double cpu_time_used_sec;
 
     // Guardar el tiempo antes de los ciclos
     start = clock();
 
-    // Ejecutar tres ciclos de un millón de iteraciones cada uno
-    for (long i = 0; i < 1000000; i++);
-    for (long i = 0; i < 1000000; i++);
-    for (long i = 0; i < 1000000; i++);
+    // Ejecutar tres ciclos de un millón de iteraciones cada uno, imprimiendo el índice
+    for (long i = 0; i < 1000000; i++) {
+        printf("Iteración: %ld en ciclo 1\n", i);
+    }
+    for (long i = 0; i < 1000000; i++) {
+        printf("Iteración: %ld en ciclo 2\n", i);
+    }
+    for (long i = 0; i < 1000000; i++) {
+        printf("Iteración: %ld en ciclo 3\n", i);
+    }
 
     // Guardar el tiempo después de los ciclos
     end = clock();
 
     // Calcular el tiempo transcurrido en segundos
-    cpu_time_used = ((double)(end - start));
-    cpu_time_used_sec = ((double)(end - start)) / CLOCKS_PER_SEC;
+    cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
 
     // Imprimir el resultado
-    printf("Tiempo de ejecución: %f \n", cpu_time_used);
-    printf("Tiempo de ejecución: %f segundos\n", cpu_time_used_sec);
+    printf("Tiempo de ejecución: %f segundos\n", cpu_time_used);
 
     return 0;
 }

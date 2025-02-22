8786d486b60ca75ed3a2283960098d3d030d66bc
Nelson Eduardo García Bravatti <104164145+nel-eleven11@users.noreply.github.com>
2025-02-21 21:49:05 -0600

    feat: exercise 4 file

--- ej4.c
+++ ej4.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+    pid_t pid = fork();  // Crear un nuevo proceso
+
+    if (pid == 0) {  
+        // Este es el proceso hijo
+        printf("Soy el proceso hijo (PID: %d)\n", getpid());
+    } else if (pid > 0) {  
+        // Este es el proceso padre
+        while (1) {
+            // Ciclo infinito en el proceso padre
+        }
+    } else {
+        // Error al hacer fork()
+        perror("Error al crear el proceso hijo");
+        return 1;
+    }
+
+    return 0;
+}
 
